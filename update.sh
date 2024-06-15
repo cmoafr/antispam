75ed679c66ad809922325d24318a245e5e12301d
cmoafr <cmoa58@gmail.com>
2024-06-15 15:11:17 +0200

    Add updater script

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Update code
+cd antispam
+git pull
+
+# Ensure venv and dependencies are good
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+
+# Restart the bot
+pkill -TERM python3
+nohup python3 bot.py &
