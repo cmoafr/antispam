e804c74f53c49e2742a04f4fd9873cc926d71784
cmoafr <cmoa58@gmail.com>
2024-07-01 00:20:40 +0200

    Add support for commands

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from logger import logger
 import os
 import discord
 from discord.ext import commands
@@ -10,8 +11,28 @@ class Bot(commands.Bot):
     def _no_prefix(bot: commands.Bot, message: discord.Message) -> str:
         return "<" if message.content.startswith(">") else ">"
     
+    async def on_ready(self):
+        await self.sync_commands()
+        logger.info(f"Logged in as {self.user}")
+    
     async def setup_hook(self) -> None:
+        self.tree.clear_commands(guild=None)
+
         # Load the cogs
         for filename in os.listdir("cogs"):
             if filename.endswith(".py"):
                 await self.load_extension(f"cogs.{filename[:-3]}")
+    
+    async def sync_commands(self):
+        await self.wait_until_ready()
+
+        # Sync local commands and copy global ones
+        for guild in self.guilds:
+            try:
+                self.tree.clear_commands(guild=guild)
+                self.tree.copy_global_to(guild=guild)
+                await self.tree.sync(guild=guild)
+            except discord.Forbidden:
+                logger.warning(f"Bot is missing permissions to sync commands in {guild.name}")
+
+        logger.info("Commands synchronized.")
